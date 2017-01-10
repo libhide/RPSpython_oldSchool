ce64eab1be23e31a30596c63c5d0bb1f429bd97a
Ratik Sharma <Ratik96@gmail.com>
2017-01-10 21:03:42 +0530

    Make code PEP8 compliant

--- RPS.py
+++ RPS.py
@@ -1,58 +1,70 @@
 from random import randint
 
-myDictionary={"1":"Rock","2":"Paper","3":"Scissor","q":"q"}
+
+myDictionary = {
+    "1": "Rock",
+    "2": "Paper",
+    "3": "Scissor",
+    "q": "q"
+}
+
 
 def displayAndGet():
     print("1.Rock\t2.Paper\t3.Scissor")
-    ch=str(input("Your Choice(Enter the number):"))
+    ch = str(input("Your Choice(Enter the number): "))
     while(ch!="1" and ch!="2" and ch!="3" and ch!="q"):
-        ch=str(input("Wrong Choice, give proper input:"))
+        ch = str(input("Wrong Choice, give proper input: "))
     return ch
 
-def decideWinner(Sys,You):
-    if(Sys.choice==You.choice):
+
+def decideWinner(Sys, You):
+    if Sys.choice == You.choice:
         return "none"
-    elif(Sys.choice=="Rock"):
-        return "You" if You.choice=="Paper" else "Sys"
-    elif(Sys.choice=="Paper"):
-        return "You" if You.choice=="Scissor" else "Sys"
-    elif(Sys.choice=="Scissor"):
-        return "You" if You.choice=="Rock" else "Sys"
-    
-class player:
+    elif Sys.choice == "Rock":
+        return "You" if You.choice == "Paper" else "Sys"
+    elif Sys.choice == "Paper":
+        return "You" if You.choice == "Scissor" else "Sys"
+    elif Sys.choice == "Scissor":
+        return "You" if You.choice == "Rock" else "Sys"
+
+
+class Player:
     ''' Applies for both human and computer'''
-    def __init__(self,operator):
-        self.operator=operator
-        self.winCounter=0
-        self.choice="playing"
+    def __init__(self, operator):
+        self.operator = operator
+        self.winCounter = 0
+        self.choice = "playing"
+
     def bout(self):
-        if(self.operator=="computer"):
-            self.choice=myDictionary[str(randint(1,3))]
+        if self.operator == "computer":
+            self.choice = myDictionary[str(randint(1, 3))]
         else:
-            self.choice=myDictionary[displayAndGet()]
+            self.choice = myDictionary[displayAndGet()]
+
     def winCount(self):
-        self.winCounter+=1
+        self.winCounter += 1
+
 
 def main():
-    YouP=player(operator="human")
-    SysP=player(operator="computer")
+    YouP = Player(operator="human")
+    SysP = Player(operator="computer")
     print("A Simple Rock-Paper-Scissor game\nYOU vs COMPUTER\n(Type q or Q to quit)")
-    while(True):
+    while True:
         YouP.bout()
-        if(YouP.choice=="q" or YouP.choice=="Q"):
+        if YouP.choice == "q" or YouP.choice == "Q":
             break
         SysP.bout()
-        print("The computer chose "+SysP.choice)
-        Win=decideWinner(SysP,YouP)
-        if(Win=="Sys"):
+        print("The computer chose {}".format(SysP.choice))
+        Win = decideWinner(SysP, YouP)
+        if Win == "Sys":
             print("You lost :(\n")
             SysP.winCount()
-        elif(Win=="You"):
+        elif Win == "You":
             print("You won :)\n")
             YouP.winCount()
         else:
             print("It's a draw!\n")
     print("==================================")
-    print("Final Score:\nYou:"+str(YouP.winCounter)+"\nComputer:"+str(SysP.winCounter))
-    exitInput=input("")
+    print("Final Score:\n You: {}\n Computer: {}".format(YouP.winCounter, SysP.winCounter))
+    exitInput = input("")
 main()
